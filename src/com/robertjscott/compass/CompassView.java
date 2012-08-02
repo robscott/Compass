9873209304dcee20246af25b567be17e082e4813
Rob Scott <rob.scott87@gmail.com>
2012-08-02 16:23:30 -0400

    removing log require

--- src/com/robertjscott/compass/CompassView.java
+++ src/com/robertjscott/compass/CompassView.java
@@ -5,7 +5,6 @@ import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 
